a6bfff143939a6dec1a51eded04556f9a9efe5fd
Alisa "Awesome" Palson and Roy van de Water <sqrt+alisa+roy@octoblu.com>
2015-04-03 09:25:07 -0700

    Make sure build succeeds before deploy

--- build_and_codedeploy.sh
+++ build_and_codedeploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./build.sh
-aws deploy create-deployment --application-name ${DEPLOY_APPLICATION_NAME} --region ${DEPLOY_REGION} --deployment-group ${DEPLOYMENT_GROUP} --revision "{\"revisionType\":\"S3\", \"s3Location\": {\"bucket\": \"${DEPLOY_BUCKET}\", \"key\": \"${DEPLOY_KEY}\", \"bundleType\": \"zip\"}}"
+./build.sh && aws deploy create-deployment --application-name ${DEPLOY_APPLICATION_NAME} --region ${DEPLOY_REGION} --deployment-group ${DEPLOYMENT_GROUP} --revision "{\"revisionType\":\"S3\", \"s3Location\": {\"bucket\": \"${DEPLOY_BUCKET}\", \"key\": \"${DEPLOY_KEY}\", \"bundleType\": \"zip\"}}"
